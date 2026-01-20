b0518ce8cac89dcb802094be4edd876c8727f649
KauePelosi <kauepelosi111215@gmail.com>
2026-01-20 19:24:52 -0300

    feat: adicionado funcao builtin cd

--- src/shell.cpp
+++ src/shell.cpp
@@ -2,15 +2,29 @@
 #include "dispatcher.hpp"
 #include "tokenize.hpp"
 #include <iostream>
+#include <limits.h>
+#include <linux/limits.h>
 #include <string>
+#include <unistd.h>
 
 void shell() {
   std::string input{};
+  char cwd[PATH_MAX];
+
   while (true) {
-    std::cout << "MiniShell-> " << std::flush;
-    std::getline(std::cin, input);
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+      std::cout << "MiniShell in " << cwd << "-> " << std::flush;
+    } else {
+      perror("getcwd() error");
+      std::cout << "MiniShell-> " << std::flush;
+    }
+    if (!std::getline(std::cin, input)) {
+      break;
+    }
 
     auto tokens = tokenize(input);
-    dispatcher(tokens);
+    if (!tokens.empty()) {
+      dispatcher(tokens);
+    }
   }
 }
