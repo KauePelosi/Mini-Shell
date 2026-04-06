d767f928827ec6b78595ad9c6bd81125b4f0cc71
Kauê Pelosi <kauepelosi111215@gmail.com>
2026-04-06 05:45:58 -0300

    refactor: replace error returns with _exit in pipelineCommands

--- src/execution/pipelineCommands.cpp
+++ src/execution/pipelineCommands.cpp
@@ -17,26 +17,32 @@ int pipelineCommands(
   size_t pipeNumber = commandCount - 1;
 
   std::vector<std::array<int, 2>> pipes(pipeNumber);
+
   for (size_t i = 0; i < pipeNumber; i++) {
     if (pipe(pipes[i].data()) == -1) {
       std::cerr << shellName << ": failed to create pipe - "
                 << std::strerror(errno) << std::endl;
-      return -1;
+      _exit(1);
     }
   }
+
   std::vector<pid_t> pids(commandCount);
+
   for (size_t i = 0; i < commandCount; i++) {
     pids[i] = fork();
 
     if (pids[i] == -1) {
       std::cerr << shellName << ": failed to create pids - "
                 << std::strerror(errno) << std::endl;
-      return -1;
+      _exit(1);
     }
 
     if (pids[i] == 0) {
       if (i > 0) {
         if (dup2(pipes[i - 1][0], STDIN_FILENO) == -1) {
+          std::cerr << shellName << ": failed to redirect STDIN - "
+                    << std::strerror(errno) << std::endl;
+          _exit(1);
         }
       }
     }
